b93dfd102267090908c3e1536979754418fdd935
PequeCeci <hey.ceci.lets.go@gmail.com>
2020-04-22 10:30:53 -0400

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
+import axios from "axios";
 
 export default function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    try {
+      const result = axios
+        .get("https://reqres.in/api/users")
+        .then((result) => setUser(result.data.data));
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <FlatList
+        data={user}
+        renderItem={({ item: userItem }) => (
+          <View>
+            <Image source={{ uri: userItem.avatar }} />
+            <View>
+              <Text>{`${userItem.first_name} ${userItem.last_name}`}</Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
 });
